4935e07c47a0010f42d952f109db52d74cf3d229
talescaue <135155634+talescaue@users.noreply.github.com>
2025-04-10 19:19:20 -0300

    Update funcao_hash.py

--- funcao_hash.py
+++ funcao_hash.py
@@ -1,1 +1,10 @@
+import hashlib
 
+def gerar_hash(texto):
+    return hashlib.sha256(texto.encode()).hexdigest()
+
+mensagem = "Integridade dos dados!"
+hash_gerado = gerar_hash(mensagem)
+
+print("Mensagem original:", mensagem)
+print("Hash SHA-256:", hash_gerado)

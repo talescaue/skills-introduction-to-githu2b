4bd70511d9086192fe8ca608a280e1f0c94c58a8
talescaue <135155634+talescaue@users.noreply.github.com>
2025-04-10 19:17:04 -0300

    criptografia_assimetrica.py

--- criptografia_assimetrica.py
+++ criptografia_assimetrica.py
@@ -1,1 +1,14 @@
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+
+# Gerar par de chaves
+private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
+public_key = private_key.public_key()
+
+# Serializar chave pública
+pem = public_key.public_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PublicFormat.SubjectPublicKeyInfo
+)
+print("Chave pública:", pem)
 
